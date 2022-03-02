1440f2ae5509ec08999b17e4b05c95863e0948c5
nocheacosador <lukaszaj@gmail.com>
2022-03-02 02:35:37 +0200

    Moved documentation from .c file to .h

--- src/MemoryAllocation.c
+++ src/MemoryAllocation.c
@@ -5,12 +5,6 @@
 static size_t s_MallocCount = 0;
 static size_t s_FreeCount = 0;
 
-/**
- * @brief stdlib malloc wrapper
- * 
- * @param size 
- * @return void* 
- */
 void* Malloc(size_t size)
 {
     void* ptr = malloc(size);
@@ -18,20 +12,12 @@ void* Malloc(size_t size)
     return ptr;
 }
 
-/**
- * @brief stdlib free wrapper
- * 
- * @param ptr 
- */
 void Free(void* ptr)
 {
     s_FreeCount++;
     free(ptr);
 }
 
-/**
- * @brief Prints succesful malloc and free call count 
- */
 void PrintMemoryAllocationReport()
 {
     printf( "[Memmory Allocation Report]\n"
--- src/MemoryAllocation.h
+++ src/MemoryAllocation.h
@@ -2,6 +2,22 @@
 
 #include <stdlib.h>
 
+/**
+ * @brief stdlib malloc wrapper
+ * 
+ * @param size 
+ * @return void* 
+ */
 void* Malloc(size_t size);
+
+/**
+ * @brief stdlib free wrapper
+ * 
+ * @param ptr 
+ */
 void Free(void* ptr);
+
+/**
+ * @brief Prints succesful malloc and free call count 
+ */
 void PrintMemoryAllocationReport();
